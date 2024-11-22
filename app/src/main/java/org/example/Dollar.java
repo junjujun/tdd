a2aca593dcef7823ebf97e99a8ae13cf14d40449
junjujun <jun@LAPTOP-N0L71D0N.>
2024-11-22 23:10:33 +0900

    page 9: refactoring 2

--- app/src/main/java/org/example/Dollar.java
+++ app/src/main/java/org/example/Dollar.java
@@ -1,12 +1,12 @@
 package org.example;
 
 public class Dollar {
-    int amount = 5 * 2;
+    int amount;
 
     Dollar(int amount) {
     }
 
     void times(int multiplier) {
-
+        amount = 5 * 2;
     }
 }
