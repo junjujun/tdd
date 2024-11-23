a5bf7e3f2ca21166d5ab437569ef0ac8d4bd0a55
junjujun <ftwlpsl@gmail.com>
2024-11-23 18:51:49 +0900

    page 81: Add testSimpleAddition (turn into red bar).

--- app/src/test/java/org/example/MoneyTest.java
+++ app/src/test/java/org/example/MoneyTest.java
@@ -14,6 +14,12 @@ class MoneyTest {
     }
 
     @Test
+    public void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
+
+    @Test
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
