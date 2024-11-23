300b6eca0eb857f92b9f693d2d287eb3dd0ba108
junjujun <ftwlpsl@gmail.com>
2024-11-23 18:44:44 +0900

    page 78: Remove unnecessary code (still green bar).

--- app/src/main/java/org/example/Dollar.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Dollar extends Money {
-
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-}
--- app/src/main/java/org/example/Franc.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Franc extends Money {
-
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-}
