f7ccf6143ccf41ab6656b226f5c2f14a218111a6
junjujun <ftwlpsl@gmail.com>
2024-11-23 18:55:01 +0900

    page 82: Implement plus method in Money class (turn into green bar).

--- app/src/main/java/org/example/Money.java
+++ app/src/main/java/org/example/Money.java
@@ -14,6 +14,10 @@ public class Money {
         return new Money(amount * multiplier, currency);
     }
 
+    Money plus(Money added) {
+        return new Money(amount + added.amount, currency);
+    }
+
     String currency() {
         return currency;
     }
