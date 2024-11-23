892e391ed950a6e2ee0377c4547ed9c77eb8f0db
junjujun <ftwlpsl@gmail.com>
2024-11-23 22:33:47 +0900

    page 101: Add testReduceMoneyDifferentCurrency (turn into red bar).

--- app/src/test/java/org/example/MoneyTest.java
+++ app/src/test/java/org/example/MoneyTest.java
@@ -47,6 +47,14 @@ class MoneyTest {
     }
 
     @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
