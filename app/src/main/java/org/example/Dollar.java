d177ea2aa9f8462c00686f2d0ff347380bb03ed0
junjujun <ftwlpsl@gmail.com>
2024-11-23 15:32:18 +0900

    page 38: Refactor Money and Dollar class for duplicate removal (turn into green bar).

--- app/src/main/java/org/example/Dollar.java
+++ app/src/main/java/org/example/Dollar.java
@@ -1,7 +1,6 @@
 package org.example;
 
-public class Dollar {
-    private int amount;
+public class Dollar extends Money {
 
     Dollar(int amount) {
         this.amount = amount;
@@ -9,11 +8,5 @@ public class Dollar {
 
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
     }
 }
