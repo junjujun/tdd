b3f4e83a76c10dcd0ce4eb83d76cf689aa4ff83c
junjujun <ftwlpsl@gmail.com>
2024-11-23 21:09:39 +0900

    page 92: Add testReduceSum (turn into red bar).

--- app/src/test/java/org/example/MoneyTest.java
+++ app/src/test/java/org/example/MoneyTest.java
@@ -32,6 +32,14 @@ class MoneyTest {
     }
 
     @Test
+    public void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
+
+    @Test
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
